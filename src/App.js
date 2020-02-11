2c55692d709795d561fc5167c6527ec0ff3703b9
keemdb <42288808+keemdb@users.noreply.github.com>
2020-02-11 09:51:32 +0900

    ver 1.1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React, { Component } from "react";
+import Counter from "./Counter";
 
 class App extends Component {
-  state = {
-    num: 0
-  };
-
-  handleIncrease = () => {
-    this.setState({
-      num: this.state.num + 1
-    });
-  };
-  handleDecrease = () => {
-    this.setState({
-      num: this.state.num - 1
-    });
-  };
-
   render() {
     return (
       <div>
-        <h1>Counter</h1>
-        <p>{this.state.num}</p>
-        <button onClick={this.handleIncrease}>추가</button>
-        <button onClick={this.handleDecrease}>감소</button>
+        <Counter />
       </div>
     );
   }
