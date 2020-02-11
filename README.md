ed7522d5ecbe220713677b5eec6de5990d6d27ee
keemdb <42288808+keemdb@users.noreply.github.com>
2020-02-11 09:14:19 +0900

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-Counter
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    num: 0
+  };
+
+  handleIncrease = () => {
+    this.setState({
+      num: this.state.num + 1
+    });
+  };
+  handleDecrease = () => {
+    this.setState({
+      num: this.state.num - 1
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter</h1>
+        <p>{this.state.num}</p>
+        <button onClick={this.handleIncrease}>추가</button>
+        <button onClick={this.handleDecrease}>감소</button>
+      </div>
+    );
+  }
+}
+
+export default App;
