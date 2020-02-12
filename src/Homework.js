c725263af76f28eb915895e2e749b2865b158e01
keemdb <keemdb@gmail.com>
2020-02-12 15:48:03 +0900

    ok

--- src/Homework.js
+++ src/Homework.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Homework extends Component {
   state = {
     userid: "",
-    userpw: ""
+    userpw: "",
   };
 
   handleChange = e => {
@@ -13,32 +13,36 @@ class Homework extends Component {
     });
   };
 
+  handlePrevent = e => {
+    e.preventDefault();
+  }
+
   render() {
     const { userid, userpw } = this.state;
     return (
       <div className="homework">
         <h1>{this.props.title}</h1>
-        <input
-          name="userid"
-          placeholder="ID"
-          value={userid}
-          onChange={this.handleChange}
-        />
-        <input
-          name="userpw"
-          placeholder="Password"
-          type="password"
-          value={userpw}
-          onChange={this.handleChange}
-        />
-        <button name="btn" type="submit">
-          Sign in
-          {
-            userpw.length < 6
-            ? console.log('ok')
-            : console.log('no')
-          }
-        </button>
+        <form onSubmit={this.handlePrevent}>
+          <input
+            name="userid"
+            placeholder="ID"
+            value={userid}
+            onChange={this.handleChange}
+          />
+          <input
+            name="userpw"
+            placeholder="Password"
+            type="password"
+            value={userpw}
+            onChange={this.handleChange}
+          />
+          <button 
+            type="submit"
+            disabled={userpw.length < 6}
+          >
+            Sign in
+          </button>
+        </form>
       </div>
     );
   }
